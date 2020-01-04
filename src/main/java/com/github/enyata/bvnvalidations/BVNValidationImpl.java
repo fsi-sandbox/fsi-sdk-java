a8dc4e26bbfd7111f3f490931622eaa779fcdc6e
Oluwatoba Aribo <>
2020-01-05 00:09:57 +0100

    Added descriptions for each objects

--- src/main/java/com/github/enyata/bvnvalidations/BVNValidationImpl.java
+++ src/main/java/com/github/enyata/bvnvalidations/BVNValidationImpl.java
@@ -10,6 +10,14 @@ import org.springframework.stereotype.Service;
 @Service
 public class BVNValidationImpl  {
 
+    /**
+     * This method generates the required Headers required for calling the /Reset endpoint
+     * @param sandBoxKey  The sandbox key is available on the FSI Portal
+     * @param organizationCode Usually provided by Nibss. For testing purposse, the value of 11111 is used. This value
+     *                         may change on production.
+     * @return HttpHeaders  Object that contains key-pair value for calling the /Reset endpoint
+     * @throws IllegalArgumentException
+     */
     public HttpHeaders getResetCredentialsHeaders(String sandBoxKey, String organizationCode) throws IllegalArgumentException {
         HttpHeaders headers = new HttpHeaders();
         if (StringUtils.isBlank(sandBoxKey) || StringUtils.isBlank(organizationCode)){
@@ -21,7 +29,15 @@ public class BVNValidationImpl  {
         return headers;
     }
 
-
+    /**
+     * The Reset credentials are usually returned in the response header after calling the /Reset endpoint
+     * Call this method and pass the response headers to help you get this required values.
+     * @param headers Contains four required key-pair values(keys include AES_KEY, Code, IVKey, PASSWORD)
+     *                that will be used for all other nibss interfaces
+     * @return ResetCredential  Object that holds thee gotten values from the headers
+     * @throws IllegalArgumentException
+     * @throws BadRemoteResponseException
+     */
     public ResetCredential getResetCredentialsFromResponseHeaders(HttpHeaders headers) throws IllegalArgumentException, BadRemoteResponseException {
 
         if (headers == null){
@@ -38,4 +54,5 @@ public class BVNValidationImpl  {
         resetCredential.setPassword(headers.get("PASSWORD").get(0));
         return resetCredential;
     }
+
 }
